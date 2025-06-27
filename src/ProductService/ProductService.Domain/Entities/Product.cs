939754b6a7454e81ee79ebc2daf297d1e9f1f48a
MohamadKalakesh <106426856+MohamadKalakesh@users.noreply.github.com>
2025-06-27 14:21:23 +0300

    Add project files.

---
+++ src/ProductService/ProductService.Domain/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace ProductService.Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; } = default!;
+        public string Description { get; private set; } = default!;
+        public decimal Price { get; private set; }
+
+        private Product() { }   //EF needs parameterless constructor to create object by reflection
+
+        public Product(string name, string description, decimal price)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+
+        public void UpdateDetails(string name, string description, decimal price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+    }
+}
