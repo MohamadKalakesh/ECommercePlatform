3a9c9800aff47e75f8eda29030f711ebb02ca5b5
MohamadKalakesh <106426856+MohamadKalakesh@users.noreply.github.com>
2025-07-14 20:00:39 +0300

    AuthService first step

---
+++ AuthService.API/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using AuthService.API.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            if (request.Username == "test" && request.Password == "test")
+                return Ok();
+            return Unauthorized();
+        }
+    }
+}
